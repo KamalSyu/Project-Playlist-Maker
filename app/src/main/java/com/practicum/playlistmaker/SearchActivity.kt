f4f497a7ea39fa698e55ae649b38456be81271d7
syuzan.qamalyan <kamalyan.syuzanna@mail.ru>
2025-10-09 11:16:28 +0400

    Changed: startActivity(intent) - deleted, icon name changed

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -14,7 +14,13 @@ class SearchActivity : AppCompatActivity() {
 
         findViewById<TextView>(R.id.back).setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            //startActivity(intent) Для выхода с экрана стоит использовать функцию finish(),
+            // а не startActivity, что создаст новую активити и откроет её.
+            // Принципиальная разница заключается в том, что в твоём коде вместо
+            // возврата открывается и создаётся новый экран с новым состоянием,
+            // а при использовании finish() закрывается текущий экран и показывается
+            // предыдущий, который был открыт перед текущим.
+
             finish()
         }
     }
